ef3e81212237013e0a56606119be8483f7dc8c14
Daniil <korko-danil@rambler.ru>
2022-04-18 19:10:19 +0300

    new file

---
+++ Korko/js/home.js
@@ -0,0 +1,36 @@
+// let variable = "istudying";
+//     study = "js";
+// console.log (variable, study)
+
+// let variable = 1000;
+//     flowers = "rose";
+// console.log(variable,flowers)
+
+// let variable = 28;
+//     variable2 = 27;
+//     variable3 = 26;
+//     variable4 = 25;
+// console.log ((variable > variable2) || (variable3 < variable4 )) 
+
+// let array = ["ilove", "JS",];
+//     console.log(array);
+
+// let object = {
+//     myschool: "Web Sevastopol",
+//     myteacher: "alex-shatokhin",
+// };
+// console.log(object);
+
+// let variable = 150;
+//     variable = variable * 2;
+// console.log(variable * 2);
+
+// let variable = 28;
+//     variable = variable - 10;
+// console.log(variable);
+
+let variable = `StartJS`;
+    name = `WebSevastopol`;
+console.log(variable,name)
+
+    
---
+++ Korko/script.js
@@ -0,0 +1,60 @@
+// let name = "Daniil";
+// let age = 25;
+// let city = "Savastopol";
+// console.log("Имя:", name);
+// console.log("Возраст:", age);
+// console.log("Город:", city);
+
+// name = "Denis";
+// age = 15;
+// city = "Moscow";
+// console.log("\n");
+// console.log("Имя:", name);
+// console.log("Возраст:", age);
+// console.log("Город:", city);
+
+// let x = 87;
+// x = x / 2 * 4;
+// alert("test" + "test111");
+
+// let name = 10;
+// alert(`privet ${name +10}`);
+
+// let answer = 45 < 12;
+// alert(answer);
+// true = да
+// false = нет
+// сравнивать числа
+// > 10>5
+// < 5 < 2
+// === 5 === 5
+// != 5 != 6
+// >=
+// == сравнение 
+// !, меняет значение на противоложный
+//&& логические и
+// ||, логический или
+// alert((5 > 4) && (10 < 11));
+// alert((5 > 6) || (10 < 11));
+// alert(!(5 > 4));
+
+// let name;
+// alert(name);
+
+// let variable = "test";
+
+// let user = {
+//     name: variable,
+//     age: 25,
+//     city: "Sevastopol",
+//     other: {
+//         second: "name",
+//     },
+
+// };
+
+// let array = ["Sasha", 25, "Sevastopol", user];
+// console.log(array);
+
+// let x = function () {};
+// console.log(typeof x);
