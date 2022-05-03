1d9f340a53b9ee024b4e96245b56ef409205f1d6
Abaz <abazzaz@yandex.ru>
2022-05-03 18:26:29 +0300

    dz

--- abaz/script2.js
+++ abaz/script2.js
@@ -7,18 +7,15 @@
 // result -=10;
 // console.log(result);
 
-
-
 // let x = 0;
 // x += 1;
 // x--;
 // x++;
 // console.log(x);
 
-
 // let name = prompt('Твой бумер');
 // if (name ==='конечно')
-// {alert('правда');} 
+// {alert('правда');}
 // else { alert('неправда');}
 
 // let result = prompt('Какой у тебя размер?');
@@ -40,9 +37,7 @@
 
 // while (x<=10){
 
-
 //     alert(x);
-
 
 //     x++;
 // }
@@ -52,10 +47,8 @@
 
 // do {
 
-
 //     alert(x);
 //     while(false);
-
 
 //     x++;
 // }
@@ -63,24 +56,17 @@
 
 // alert(x);
 
-
 // for (let i = 0; i < 100; i++){
 
 // if (i === 5) {
 
 // break;
 
-
-
 // }
-
-
 
 //     alert(i);
 
-
 // }
-
 
 //  for (let i = 0; i <= 5; i++){
 
@@ -88,17 +74,11 @@
 
 //  continue;
 
-
-
 //  }
-
 
 //      alert(i);
 
 //  }
-
-
-
 
 /*
  let y = Math.floor(Math.random() * 100);
@@ -168,7 +148,7 @@ for(let i = 0; i <= 100; i++) {
 //         alert(`Умноженое значение ${x}` );
 //     }
 //     if(k == 'Нет')break;
-    
+
 // }
 
 // let y = 0;
@@ -176,8 +156,6 @@ for(let i = 0; i <= 100; i++) {
 //  y = y + i;
 // }
 //  alert(y);
-
-
 
 //  let n = 1000;
 //  let num = 0;
@@ -197,9 +175,7 @@ for(let i = 0; i <= 100; i++) {
 // alert('1');
 // }
 
-
 // массивы и циклы
-
 
 // let array = [ [0,1,2,3,4,],
 // [5,6,7,8,9,],
@@ -213,12 +189,9 @@ for(let i = 0; i <= 100; i++) {
 
 // for(let j = 0; j < temp.length; j++){
 
-
 //     console.log(temp[j]);
 // }
 // }
-
-
 
 // let array = [];
 // let count = 0;
@@ -226,16 +199,12 @@ for(let i = 0; i <= 100; i++) {
 // for(let i = 0; i < 5; i++){
 //     array[i] = [];
 //     for(let j = 0; j < 5; j++){
-    
+
 //     count++;
 //         array[i][j] = count;
 //     }
 //     }
 // console.log(array);
-
-
-
- 
 
 //      let array = [];
 //      let count = 0;
@@ -244,15 +213,15 @@ for(let i = 0; i <= 100; i++) {
 //         array[i] = [];
 
 //     for(let j = 0; j < 9; j++){
-        
+
 //         array [i][j] = 0;
-          
+
 //          if(j ==(8-i)){
-              
+
 //         array[i][j]=1;}
-         
+
 //         if (i == j){
-     
+
 //         array [i][j] = 1;}
 //     }
 //      // count++;
@@ -264,13 +233,12 @@ for(let i = 0; i <= 100; i++) {
 // arr.length = 1;
 // alert(arr[1]);
 
-
 // var arr = [];
 // alert(arr.length);
- 
+
 //  arr[2] = 23;
 //  alert(arr.length);
- 
+
 //  arr[3] = 13;
 //  alert(arr.length);
 //  КОНСТРУКЦИЯ SWITCH
@@ -295,12 +263,7 @@ for(let i = 0; i <= 100; i++) {
 //         alert('не ягода')
 //         break;
 
-
-
-
-
 // }
-
 
 // функция
 //   function getRandomNumber(x){
@@ -314,20 +277,14 @@ for(let i = 0; i <= 100; i++) {
 
 //      for(let j = 0; j < 9; j++){
 //          // array [i][j] = 0;
-    
+
 //           array[i][j] = getRandomNumber(10);
-    
+
 //      }
-
-
 
 //      }
 
 //  console.log(array);
-
-
-
-
 
 // function getRandomNumber(x = 10){
 //       return Math.floor(Math.random() * x);
@@ -335,19 +292,17 @@ for(let i = 0; i <= 100; i++) {
 
 // console.log (getRandomNumber(100));
 
-
-
 // function min(a,b) {
 
 //     // return Math.floor(Math.random() * x);
-    
+
 //     if (a < b) {
 //         return a;
 //     }else{
 //         return b;
 //     }
 // }
- 
+
 // console.log(min(100 * 2,150 ));
 
 /*
@@ -378,109 +333,99 @@ for(let i = 0; i <= 100; i++) {
   }
 
   */
-              //  Задания № 1
+//  Задания № 1
 
-  let numb1 = prompt('Введите первое число');
+let numb1 = prompt("Введите первое число");
 
-   let znak = prompt('Действие: /,-,*,+');
+let znak = prompt("Действие: /,-,*,+");
 
-   let numb2 = prompt('Введите второе число');
+let numb2 = prompt("Введите второе число");
 
-      alert(calc(numb1,znak,numb2));
-   let numb3 = calc(numb1,znak,numb2);
+alert(calc(numb1, znak, numb2));
+let numb3 = calc(numb1, znak, numb2);
 
-
-  while(1){
-    if(confirm('Продолжаем работу с числом? (Да/Нет)')){
-      let znak = prompt('Выбери действие: /,-,*,+');
-      let numb2 = prompt('Введите второе число');
-      alert(calc(numb3,znak,numb2));
-      numb3 = calc(numb3,znak,numb2);
-     }
-     else {
-    
-     alert('Слабак');
-     break;
-    }
-  
-    if (numb3 === 100) {
-     alert('Ты попал в ловушку ХА ХА ХА!!!');
-        // let i = 0;
-// //       do{
-// //           let a = prompt('Сколько ног и рук у осминога? Ответ:?');
-// //             i++;
-// //             alert(a);
-// //       }while(a === 8);
-    }
-      else 
-//       {        // alert('Пока');break;  
-      alert('ТЫ НА ВСЕГДА ОСТАНЕШЬСЯ ЗДЕСЬ!!!');
-//       }
+while (1) {
+  if (confirm("Продолжаем работу с числом? (Да/Нет)")) {
+    let znak = prompt("Выбери действие: /,-,*,+");
+    let numb2 = prompt("Введите второе число");
+    alert(calc(numb3, znak, numb2));
+    numb3 = calc(numb3, znak, numb2);
+  } else {
+    alert("Слабак");
+    break;
   }
+
+  if (numb3 === 100) {
+    alert("Ты попал в ловушку ХА ХА ХА!!!");
+    // let i = 0;
+    // //       do{
+    // //           let a = prompt('Сколько ног и рук у осминога? Ответ:?');
+    // //             i++;
+    // //             alert(a);
+    // //       }while(a === 8);
+  }
+  //       {        // alert('Пока');break;
+  else alert("ТЫ НА ВСЕГДА ОСТАНЕШЬСЯ ЗДЕСЬ!!!");
+  //       }
+}
 //       // if (numb3 === (100 - k)){
-    
+
 //      // let k = (20);
-     
+
 //      // alert("Ты все ближе к сокровищам !!!");
 
 // //     // }
-     function calc(x,y,z){
-    let result=0;
-    switch (y) {
-      case '+':
-        result = Number(x) + Number(z);
-        break;
-      case '-':
-        result = x - z;
-        break;
-      case '*':
-        result = x * z;
-        break;
-     case '/':
-       result = x / z;
-       break;
-    }
-    return result;
-     }
+function calc(x, y, z) {
+  let result = 0;
+  switch (y) {
+    case "+":
+      result = Number(x) + Number(z);
+      break;
+    case "-":
+      result = x - z;
+      break;
+    case "*":
+      result = x * z;
+      break;
+    case "/":
+      result = x / z;
+      break;
+  }
+  return result;
+}
 
-    //  сортировка массивов по возраст Задания 2
+//  сортировка массивов по возраст Задания 2
 
-  let arr = [5, 2, 1,18, -10, 8,-1,123];
+let arr = [5, 2, 1, 18, -10, 8, -1, 123];
 
-   arr.sort((a, b) => a - b);
- 
-   alert( arr );
+arr.sort((a, b) => a - b);
 
+alert(arr);
 
+//  Задания 2
 
+let a = [8, -1, 35, -23, 7, 10, 0];
+// let b = [1, 2, 3, 4, 7, 10, 110];
 
-
-
-
-  //  Задания 2 
-
-let a = [8, -1, 35, -23,7,10,0];
-let b = [1, 2, 3, 4,7,10,110];
-
-function swap(ar,x,y){
-let j = ar[y];
+function swap(ar, x, y) {
+  let j = ar[y];
   ar[y] = ar[x];
   ar[x] = j;
 }
 
-function sort(k){
-  let n=1;
-while(n){
-  for(let j = 1; j < k.length;j++){
-    if(k[j-1] > k[j]){
-      swap(k,j,j-1);
-      n++;
+function sort(k) {
+  let n = 1;
+  while (n) {
+    for (let j = 1; j < k.length; j++) {
+      if (k[j - 1] > k[j]) {
+        swap(k, j, j - 1);
+        n++;
+      }
     }
-
+    if (n > 1) { n = 1;
+    } else { n = 0;
+    }
   }
-  if(n>1)n=1;
-  else n=0;
-}
 }
 
 alert(a);
