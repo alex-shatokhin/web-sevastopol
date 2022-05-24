21a393383b9f76a1c5e5d1e444c125be6997062c
Daniiil25 <korko-danil@rambler.ru>
2022-05-24 16:51:27 +0300

    homework Korko

--- Korko/js/home.js
+++ Korko/js/home.js
@@ -1,36 +1,116 @@
-// let variable = "istudying";
-//     study = "js";
-// console.log (variable, study)
 
-// let variable = 1000;
-//     flowers = "rose";
-// console.log(variable,flowers)
 
-// let variable = 28;
-//     variable2 = 27;
-//     variable3 = 26;
-//     variable4 = 25;
-// console.log ((variable > variable2) || (variable3 < variable4 )) 
+// alert( userName ); // 
 
-// let array = ["ilove", "JS",];
-//     console.log(array);
+// showMessage();
 
-// let object = {
-//     myschool: "Web Sevastopol",
-//     myteacher: "alex-shatokhin",
+// alert( userName );
+
+// function sum(a, b) {
+//     return a + b;
+//   }
+//   let result = sum(1, 2);
+//   alert( result ); // 3
+
+// function getName() {
+//     var name = document.getElementById("input_name").value;
+//   }
+  
+//   function getAge() {
+//     var age = document.getElementById("input_age").value;
+//   }
+//   console.log (getName)
+//   console.log (getAge)
+
+// let user = {
+//     name: "Вася"
+//   };
+  
+//   let id = Symbol("id");
+  
+//   user[id] = 1;
+  
+//   alert( user[id] );
+
+
+// let str = 'Hi';
+
+// str = 'h' + str[0]; // заменяем строку
+
+// alert( str ); // hi
+
+//home 
+
+// let truncate = ("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…";
+
+// let truncate = ("Всем привет!", 20) = "Всем привет!";
+
+// function truncate(str, maxlength) {
+//     return(str.length > maxlength)?
+//     str.slice(0, maxlength - 1) + "..." : str;
+// }
+// alert(truncate)
+
+
+
+// function ucFirst(str) {
+//     if (!str) return str;
+  
+//     return str[0].toUpperCase() + str.slice(1);
+//   }
+  
+//   alert( ucFirst("в") );
+
+
+
+
+// function CheckSpam(str){
+//     let LowerStr = str.toLowerCase();
+
+// }
+
+
+
+
+// const truncate = function (str, maxlength) {
+//     let str_new = "";
+//     if (str.length > maxlength) {
+//       for (let i = 0; i < maxlength - 3; i++) {
+//         str_new += str[i];
+//       }
+//       for (let i = 0; i < 3; i++) str_new = str_new + ".";
+//       return str_new;
+//     }
+//     return str;
+//   };
+//   let str = "123456789";
+//   alert(truncate(str, 2));
+
+
+
+// const ucfirst = function (str) {
+//     let str_c = str[0].toUpperCase();
+//     for (let i = 1; i < str.length; i++) str_c += str[i];
+//     return str_c;
+//   };
+//   alert(ucfirst("qwerty"));
+
+
+// const readNumber = function () {
+//     let number = prompt("Введите число:  ");
+//     return number;
+//   };
+//   alert(readNumber());
+
+/* Задание 2 спам лист */
+// let spamlist = ["Viagra", "XXX"];
+// const checkSpam = function (str) {
+//   let str_lower = str.toLowerCase();
+//   //alert(str.toLowerCase());
+//   for (let i in spamlist) {
+//     if (str_lower.includes(spamlist[i].toLowerCase())) return true;
+//     //if(str.indexOf(spamlist[i]) str.toLowerCase());
+//   }
+//   return false;
 // };
-// console.log(object);
-
-// let variable = 150;
-//     variable = variable * 2;
-// console.log(variable * 2);
-
-// let variable = 28;
-//     variable = variable - 10;
-// console.log(variable);
-
-let variable = `StartJS`;
-    name = `WebSevastopol`;
-console.log(variable,name)
-
-    
+//alert(checkSpam("By ViaGRa now"));
--- Korko/script.js
+++ Korko/script.js
@@ -1,46 +1,109 @@
 
 
 
-let nu1 = prompt("Введи первое число");
-let nu2 = prompt("Выберете действие: /, +, *, -");
-let nu3 = prompt("Введи второе число");
+// let nu1 = prompt("Введи первое число");
+// let nu2 = prompt("Выберете действие: /, +, *, -");
+// let nu3 = prompt("Введи второе число");
 
-alert (calc(nu1,nu2,nu3));
+// alert (calc(nu1,nu2,nu3));
 
-let s = calc(nu1,nu2,nu3);
-while(1){if(confirm("продолжаем работу с числом? да/нет"));
-let nu2 = prompt("Выберете действие: /, +, *, -");
-let nu3 = prompt("Введи второе число");
-alert(calc(s,nu2,nu3));
-s = calc(s,nu2,nu3);
+// let s = calc(nu1,nu2,nu3);
+// while(1){if(confirm("продолжаем работу с числом? да/нет"));
+// let nu2 = prompt("Выберете действие: /, +, *, -");
+// let nu3 = prompt("Введи второе число");
+// alert(calc(s,nu2,nu3));
+// s = calc(s,nu2,nu3);
 
-}
-
-
+// }
 
 
 
 
 
 
-function calc (x,y,b){
-let s = 0;
-switch(y) {
-    case'-':
-     s = x - b;
-    break;
-    case'+':
-     s = Number(x) + Number(b);
-    break;   
-    case'*':
-     s = x * b;
-    break;
-    case'/':
-     s = x / b;
-    break;
-   }
+
+
+// function calc (x,y,b){
+// let s = 0;
+// switch(y) {
+//     case'-':
+//      s = x - b;
+//     break;
+//     case'+':
+//      s = Number(x) + Number(b);
+//     break;   
+//     case'*':
+//      s = x * b;
+//     break;
+//     case'/':
+//      s = x / b;
+//     break;
+//    }
   
-   return (s);
+//    return (s);
    
-}
+// }
 
+//  const x = function (a, b) {
+//      return a*b;
+//  } 
+//  console.log (2, 2)
+
+// const pow = (a, b) => {
+//     if (a === 0) {
+//        return 1; 
+//     }
+//     return a * pow(a, b - 1);
+// }
+
+// let func = function () {
+//     return "текст";
+// }
+// let func = () => "текст"; //аналог первой записи
+
+// let func = a => a * 100;
+// alert (a);
+
+// let func = (question, yes, no) => ? 
+// question, 
+
+// let key = prompt(`введи ключ`);
+
+// const user = {
+//     [key]: `гена`,
+// };
+
+// console.log(user);
+
+
+//ДЗ 10.05.2022
+
+// let user = {
+//     name: "Daniil",
+//     age: 28,
+// };
+// let clone = {}
+
+// for (let key in user) {
+//     clone[key] = user[key]
+// }
+// alert(user.name)
+ 
+//11.05
+
+
+// функция конструктор 
+
+// д/з
+// function User(name, weight, price, quantity){
+//     this.name = name;
+//     this.weight = weight;
+//     this.price = price;
+//     this.quantity = quantity;
+//     this.getName = function () {
+//         alert(this.name);
+    
+        
+//     }
+// }
+// let user = new User();
