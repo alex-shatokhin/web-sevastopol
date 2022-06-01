378b14ed656042e50a1599f7e1094b75111023da
Aleksandr Shatokhin <a.shatohin@hh.ru>
2022-06-01 18:51:42 +0300

    01.06

---
+++ shatokhin/01.06.2022.js
@@ -0,0 +1,26 @@
+// метод map
+
+// let number = [2, 3, 4, 5, 6, 7];
+//
+// let number2 = number.map((num) => num * num);
+//
+// console.log(number, number2);
+
+// console.log([1, -2, 15, 2, 0, 8].sort( (a, b) => a - b ));
+
+// let arr = '192.168.0.1:8000:user:12345'.split(':');
+//
+// console.log(arr);
+//
+// Array.isArray(arr);
+
+// const array = [1, 2, 3, 4, 5];
+//
+// const even = (element) => element % 2 === 0;
+//
+// console.log('Хотя бы 1', array.some(even));
+//
+// console.log('Каждый', array.every(even));
+
+// let arr = [1, 2, 3].fill(0, 0, 3);
+// console.log(arr);
